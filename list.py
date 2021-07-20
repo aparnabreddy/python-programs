e9c5b26d4565247dc67706b696ebc266c678105e
aparnabreddy <aparnabreddy26@gmail.com>
2021-07-20 15:45:02 +0530

    List updated

--- list.py
+++ list.py
@@ -1,4 +1,12 @@
 names=["Aparna", "Impana", "Ajay", "Darshan", 23, 22, 21, 20]
 print(names)
+del names[4]
+print(names)
+names.remove("Darshan")
+print(names)
+names.pop(1)
+print(names)
 print(names[0])
 print(names[2:5])
+names.clear()
+print(names)
